f9c30edabd42f703b09be8def3f76e9cb55debc8
hum-motor-whir <rodrigo.bucheli@gmail.com>
2021-10-23 01:34:47 -0400

    Working index and create routes

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const express = require("express");
 const app = express();
 // import mongoose
 const mongoose = require("mongoose");
+// import middleware
+const cors = require("cors");
+const morgan = require("morgan");
 
 // DATABASE CONNECTION //
 // Establish Connection
@@ -22,10 +25,81 @@ mongoose.connection
   .on("open", () => console.log("You are connected to mongoose"))
   .on("close", () => console.log("You are disconnected from mongoose"))
   .on("error", (error) => console.log(error));
+
+// MODELS //
+const UnitSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  personal: String,
+  crest: String,
+  baseStats: {
+    hp: Number,
+    str: Number,
+    mag: Number,
+    dex: Number,
+    spd: Number,
+    lck: Number,
+    def: Number,
+    res: Number,
+    cha: Number,
+  },
+  maxStats: {
+    hp: Number,
+    str: Number,
+    mag: Number,
+    dex: Number,
+    spd: Number,
+    lck: Number,
+    def: Number,
+    res: Number,
+    cha: Number,
+  },
+  growthRates: {
+    hp: Number,
+    str: Number,
+    mag: Number,
+    dex: Number,
+    spd: Number,
+    lck: Number,
+    def: Number,
+    res: Number,
+    cha: Number,
+  }
+});
+
+const Unit = mongoose.model("Unit", UnitSchema);
+
+// MIDDLEWARE //
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
+
 // ROUTES //
 // Test Route
 app.get("/", (req, res) => {
-    res.send("hello world");
+  res.send("hello world");
+});
+
+// Index
+app.get("/units", async (req, res) => {
+  try {
+      // send all people
+      res.json(await Unit.find({}));
+  } catch (error) {
+      //send error
+      res.status(400).json(error);
+  }
+});
+
+// Create
+app.post("/units", async (req, res) => {
+  try {
+      // send all units
+      res.json(await Unit.create(req.body));
+  } catch (error) {
+      //send error
+      res.status(400).json(error);
+  }
 });
 
 // LISTENER //
